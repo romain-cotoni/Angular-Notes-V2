4d05089487b39a134fabba2aa1b6fd4b78bc15da
romain-cotoni <romain.cotoni@gmail.com>
2024-09-18 23:08:39 +0200

    environment http://vps-a1453f27.vps.ovh.net:8080/api

--- environment.ts
+++ environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://notes-write.ovh:8080/api' // vps
+    apiUrl: 'http://vps-a1453f27.vps.ovh.net:8080/api' // vps
     //apiUrl: 'http://localhost:8080/api' // localhost
 };
 
